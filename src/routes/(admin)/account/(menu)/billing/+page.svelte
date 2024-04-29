fe7d71d9a265829eacdfe99680c49e6e28a9850f
ngacho <brandonngacho@gmail.com>
2024-04-29 17:59:37 -0400

    cancel subscription made easy

--- src/routes/(admin)/account/(menu)/billing/+page.svelte
+++ src/routes/(admin)/account/(menu)/billing/+page.svelte
@@ -7,16 +7,33 @@
     pricingPlans,
     defaultPlanId,
   } from "../../../../(marketing)/pricing/pricing_plans"
+  import { applyAction, enhance } from "$app/forms"
+  import type { SubmitFunction } from "@sveltejs/kit"
 
   let adminSection: Writable<string> = getContext("adminSection")
   adminSection.set("billing")
 
   export let data
+  // Page state
+  let loading = false
+  let showSuccess = false
 
   let currentPlanId = data.currentPlanId ?? defaultPlanId
   let currentPlanName = pricingPlans.find(
     (x) => x.id === data.currentPlanId,
   )?.name
+
+  const handleSubmit: SubmitFunction = () => {
+    loading = true
+    return async ({ update, result }) => {
+      await update({ reset: false })
+      await applyAction(result)
+      loading = false
+      if (result.type === "success") {
+        showSuccess = true
+      }
+    }
+  }
 </script>
 
 <svelte:head>
@@ -38,17 +55,20 @@
     </div>
   {/if}
 {:else}
-  <SettingsModule
-    title="Subscription"
-    editable={false}
-    fields={[
-      {
-        id: "plan",
-        label: "Current Plan",
-        initialValue: currentPlanName || "",
-      },
-    ]}
-    editButtonTitle="Manage Subscripton"
-    editLink="/account/billing/manage"
-  />
+  <div class="card p-6 pb-7 mt-8 max-w-xl flex flex-col md:flex-row shadow">
+    <form class="form-widget flex flex-col" method="POST" use:enhance={handleSubmit}>
+      <div>
+        <div class="text-xl font-bold mb-3 w-48 flex-none">Subscription</div>
+        <div class="w-full min-w-48">
+          <div class="mb-6">
+            <div class="text-sm text-gray-600">Current Plan</div>
+            <div class="text-lg font-bold">{currentPlanName}</div>
+          </div>
+          <button class="btn btn-outline btn-sm min-w-[145px]" disabled={loading}>
+            Cancel Subscription
+          </button>
+        </div>
+      </div>
+    </form>
+  </div>
 {/if}
