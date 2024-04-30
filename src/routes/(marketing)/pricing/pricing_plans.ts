aa90c90e1e730e07d451939b99b855b2939dd4f2
ngacho <brandonngacho@gmail.com>
2024-04-30 18:44:24 -0400

    fix language pricing

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -11,22 +11,21 @@ export const pricingPlans = [
     stripe_price_id: "price_1PAipH018unCjwxRnInTDsgA",
     stripe_product_id: "prod_Q0kJdqOUnL3Yek",
     features: [
-      "Get email summary",
-      "Add tasks from your email to your calendar in a single click"
+      "Summarize your emails and quickly understand what's important",
+      "Extract action items and to-dos from emails and schedule them as events directly from your email",
     ],
   },
   {
     id: "pro-yearly",
-    name: "Annual Pro",
-    description:
-      "Subscribe annually for full access, it's 15% cheaper",
+    name: "Annual Pro - 15% OFF",
+    description: "Subscribe annually for full access",
     price: "$49.99",
     priceIntervalName: "per year",
     stripe_price_id: "price_1PAipb018unCjwxRRXgHjZ68",
     stripe_product_id: "prod_Q0kJdqOUnL3Yek",
     features: [
-      "Everything is free",
-      "15% off",
+      "Summarize your emails and quickly understand what's important",
+      "Extract action items and to-dos from emails and schedule them as events directly from your email",
     ],
   },
 ]
