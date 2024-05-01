b48423c9b40c9a463db11bacc3b4abbbb5cd9304
ngacho <brandonngacho@gmail.com>
2024-05-01 12:08:52 -0400

    fix pricing and production

--- src/routes/(marketing)/pricing/pricing_plans.ts
+++ src/routes/(marketing)/pricing/pricing_plans.ts
@@ -8,8 +8,8 @@ export const pricingPlans = [
       "Subscribe monthly and get access to all the features. Cancel anytime.",
     price: "$4.99",
     priceIntervalName: "per month",
-    stripe_price_id: "price_1PBRqNA7HIDVdCI741qoqn7e",
-    stripe_product_id: "prod_Q1UqnTYHt545gU",
+    stripe_price_id: "price_1PAipH018unCjwxRnInTDsgA",
+    stripe_product_id: "prod_Q0kJdqOUnL3Yek",
     features: [
       "Summarize your emails and quickly understand what's important",
       "Extract action items and to-dos from emails and schedule them as events directly from your email",
@@ -21,8 +21,8 @@ export const pricingPlans = [
     description: "Subscribe annually for full access",
     price: "$49.99",
     priceIntervalName: "per year",
-    stripe_price_id: "price_1PBRqNA7HIDVdCI74noDytd0",
-    stripe_product_id: "prod_Q1UqnTYHt545gU",
+    stripe_price_id: "price_1PAipb018unCjwxRRXgHjZ68",
+    stripe_product_id: "prod_Q0kJdqOUnL3Yek",
     features: [
       "Summarize your emails and quickly understand what's important",
       "Extract action items and to-dos from emails and schedule them as events directly from your email",
