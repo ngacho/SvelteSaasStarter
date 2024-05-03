ae345222cd706acd92eb87a295856d8679b3e484
rodas-j <50768698+rodas-j@users.noreply.github.com>
2024-05-02 22:30:14 -0400

    Update Dockerfile and compose.yaml to use port 10000 instead of 3000

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,10 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 3000
-},
-preview: {
+    port: 10000,
+  },
+  preview: {
     host: true,
-    port: 3000
-} 
-
+    port: 10000,
+  },
 })
