773e22ab600555a69fde6ed4fea781c1c01568aa
ngacho <brandonngacho@gmail.com>
2024-05-03 12:09:21 -0400

    padding for small screens

--- src/routes/(marketing)/privacy/+page.svelte
+++ src/routes/(marketing)/privacy/+page.svelte
@@ -8,7 +8,7 @@
 </svelte:head>
 
 <div class="">
-  <div class="max-w-3xl mx-auto my-6">
+  <div class="max-w-3xl mx-auto my-6 p-3">
     <h1 class="text-4xl my-2 font-extrabold">Privacy Policy</h1>
     <div class="sm:mx-2">
       <p>
